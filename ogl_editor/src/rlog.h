ae0d0a0671826239ad3fd13687cede6ddf1f4633
Daniel Collin <daniel@collin.com>
2012-10-26 16:37:07 +0200

    Added code for logging

---
+++ ogl_editor/src/rlog.h
@@ -0,0 +1,14 @@
+#pragma once
+
+enum  
+{
+    R_DEBUG, 
+    R_INFO, 
+    R_ERROR, 
+};
+
+void rlog(int logLevel, const char* format, ...);
+void rlog_set_level(int logLevel);
+void rlog_level_push();
+void rlog_level_pop();
+
