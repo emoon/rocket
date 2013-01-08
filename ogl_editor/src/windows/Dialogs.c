8eea4757657559bd611fdea58061598bccb33508
Daniel Collin <daniel@collin.com>
2013-01-08 15:59:58 +0100

    Added Save and ColorPick dialogs

--- ogl_editor/src/windows/Dialogs.c
+++ ogl_editor/src/windows/Dialogs.c
@@ -23,16 +23,29 @@ int Dialog_open(wchar_t* path, int pathSize)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int Dialog_save(char* path, int pathSize)
+int Dialog_save(wchar_t* path, int pathSize)
 {
-	(void)path;
+	OPENFILENAME dialog;
 	(void)pathSize;
-	return 0;
+	ZeroMemory(&dialog, sizeof(dialog));
+	dialog.lStructSize = sizeof(dialog);
+	dialog.lpstrFilter = L"Rocket (*.rocket)\0*All Files (*.*)\0*.*\0";
+	dialog.lpstrFile = path;
+	dialog.nMaxFile = MAX_PATH;
+	dialog.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
+	dialog.lpstrDefExt = L"rocket";
+	return GetSaveFileName(&dialog);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void Dialog_showColorPicker(unsigned int* color)
 {
-	(void)color;
+	CHOOSECOLOR cc;
+	ZeroMemory(&cc, sizeof(CHOOSECOLOR));
+	cc.lStructSize = sizeof(CHOOSECOLOR);
+	cc.rgbResult = *color;
+	cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+	if (ChooseColor(&cc)) 
+		*color = cc.rgbResult;
 }
