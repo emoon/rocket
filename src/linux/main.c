2bd97c8306716ba7eeb1e5af2eb11e3ec0de5706
Vito Caputo <vcaputo@pengaru.com>
2023-01-22 00:41:53 -0800

    linux: SDL_GL_DOUBLEBUFFER is not a valid CreateWindow flag (#164)
    Passing this as a flag is triggering an unexpected modeswitch,
    probably because the value maps to the fullscreen flag.

--- src/linux/main.c
+++ src/linux/main.c
@@ -423,7 +423,7 @@ int main(int argc, char* argv[]) {
 
     // SDL_EnableKeyRepeat(200, 20);
     window = SDL_CreateWindow("Rocket", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600,
-                              SDL_WINDOW_OPENGL | SDL_GL_DOUBLEBUFFER | SDL_WINDOW_RESIZABLE);
+                              SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
 
     if (!window) {
         fprintf(stderr, "SDL_CreateWindow(): %s\n", SDL_GetError());
